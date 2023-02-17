beaa144c1dadfcb9dfcd01fc49577300a6c85b28
@Snick55 <snikerss123321@gmail.com>
2023-02-17 20:30:27 +0300

    added save city in room(domain layer)

--- app/src/main/java/com/snick/weather/currentWeather/domain/CityDomain.kt
+++ app/src/main/java/com/snick/weather/currentWeather/domain/CityDomain.kt
@@ -1,5 +1,12 @@
 package com.snick.weather.currentWeather.domain
 
+import com.snick.weather.currentWeather.data.CityData
+import com.snick.weather.currentWeather.presentation.CityUi
+
 data class CityDomain(
     private val name: String
-)
+){
+    fun toData() = CityData(name)
+
+    fun toUi() = CityUi.City(name)
+}
--- app/src/main/java/com/snick/weather/currentWeather/domain/WeatherInteractor.kt
+++ app/src/main/java/com/snick/weather/currentWeather/domain/WeatherInteractor.kt
@@ -1,11 +1,19 @@
 package com.snick.weather.currentWeather.domain
 
+import com.snick.weather.currentWeather.data.CityData
 import com.snick.weather.currentWeather.data.CurrentWeatherRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 interface WeatherInteractor {
 
     suspend fun fetchWeather(city: String): CurrentWeatherDomain
+
+    fun getSavedCities(): Flow<List<CityDomain>>
+
+    suspend fun saveCity(cityDomain: CityDomain)
+
+    suspend fun deleteCity(cityDomain: CityDomain)
 
 
     class Base @Inject constructor (
@@ -14,6 +22,18 @@ interface WeatherInteractor {
 
         override suspend fun fetchWeather(city: String): CurrentWeatherDomain {
             return repository.fetchCurrentWeather(city)
+        }
+
+        override fun getSavedCities(): Flow<List<CityDomain>> {
+            return repository.getSavedCities()
+        }
+
+        override suspend fun saveCity(cityDomain: CityDomain) {
+            repository.saveCity(cityDomain.toData())
+        }
+
+        override suspend fun deleteCity(cityDomain: CityDomain) {
+            repository.deleteCity(cityDomain.toData())
         }
     }
 }
